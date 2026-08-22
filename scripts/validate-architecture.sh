e0574fc7689423031031ae8d899809f04ada0974
Jim Thompson <jimthompson5802@gmail.com>
2026-08-22 18:59:20 -0400

    feat: update validate-architecture.sh to use base_url_host for architecture file validation

--- scripts/validate-architecture.sh
+++ scripts/validate-architecture.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+base_url_host=${1:-http://my-calm.repo:8080}
+
 set -x
 
 printf "\n\nValidating top level CALM architecture files with valid detailed architectures...NO ERRORS EXPECTED\n"
-calm validate -a http://localhost:8080/architectures/calm-3.json
+calm validate -a ${base_url_host}/architectures/calm-3.json -f pretty
 
 printf "\n\nValid detailed architecture file...NO ERRORS EXPECTED\n"
-calm validate -a http://localhost:8080/architectures/calm-hub-detail.architecture.json
+calm validate -a ${base_url_host}/architectures/calm-hub-detail.architecture.json -f pretty
 
 printf "\n\n\nValid top-level architecture that references a detailed architecture with an error in it. No Errors flagged\n"
-calm validate -a http://localhost:8080/architectures/calm-3-ref-bad.json
+calm validate -a ${base_url_host}/architectures/calm-3-ref-bad.json -f pretty
 
 printf "\n\n\nDetailed architecture with an error in it. ERRORS EXPECTED\n"
-calm validate -a http://localhost:8080/architectures/calm-hub-detail.architecture-bad.json
+calm validate -a ${base_url_host}/architectures/calm-hub-detail.architecture-bad.json -f pretty
 
 printf "\n\n\nValidate architecture against pattern/standards NO ERRORS EXPECTED\n"
-calm validate -a http://localhost:8080/architectures/generated-webapp.json -p http://localhost:8080/patterns/company-base-pattern.json 
+calm validate -a ${base_url_host}/architectures/generated-webapp.json -p ${base_url_host}/patterns/company-base-pattern.json -f pretty
 
 printf "\n\n\nValidate architecture with Controls.  NO ERRORS EXPECTED\n"
-calm validate -a http://localhost:8080/architectures/ecommerce-platform.json 
+calm validate -a ${base_url_host}/architectures/ecommerce-platform.json -f pretty
