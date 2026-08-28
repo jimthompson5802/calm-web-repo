09b2657bdb609f6e1cfe87711a3d58f482525f99
Jim Thompson <jimthompson5802@gmail.com>
2026-08-28 12:31:36 -0400

    feat: add logging for config path and request details in DirectUrlAuthPlugin

--- custom-idp/v1/src/direct-url-auth.ts
+++ custom-idp/v1/src/direct-url-auth.ts
@@ -5,6 +5,7 @@ type AuthConfig = {
 };
 
 export default class DirectUrlAuthPlugin {
+    private readonly configPath: string;
     private readonly configPromise: Promise<AuthConfig>;
 
     constructor(configPath?: string) {
@@ -12,6 +13,7 @@ export default class DirectUrlAuthPlugin {
             throw new Error('configPath is required');
         }
 
+        this.configPath = configPath;
         this.configPromise = readFile(configPath, 'utf8').then((text) =>
             JSON.parse(text) as AuthConfig
         );
@@ -19,6 +21,13 @@ export default class DirectUrlAuthPlugin {
 
     async getAuthHeaders(_url: string, _requestBody: unknown): Promise<Record<string, string>> {
         const config = await this.configPromise;
+
+        // write to stdout the configpath, _url and _requestBody
+        console.log('>>>configPath:', this.configPath);
+        console.log('>>>config:', this.configPromise);
+        console.log('>>>url:', _url); 
+        console.log('>>>requestBody:', _requestBody);
+
 
         if (!config.fakeToken) {
             throw new Error('fakeToken is required');
